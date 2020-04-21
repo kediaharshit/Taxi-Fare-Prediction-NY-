f6bd48eb2f1fefc3358029cab6ff272f5816e4f0
Harshit Kedia <kediaharshit9@gmail.com>
2020-04-21 14:57:05 +0530

    date - time formatted to timestamp

--- tensor_test.py
+++ tensor_test.py
@@ -16,8 +16,15 @@ from matplotlib import gridspec
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+import datetime as dt
 from sklearn import metrics
 import tensorflow as tf
 from tensorflow.python.data import Dataset
 
 data_set = pd.read_csv('train.csv', sep = ',', nrows = 100);
+data_set.pickup_datetime = pd.DatetimeIndex(data_set.pickup_datetime).asi8 // 10**9
+input = data_set.to_numpy()
+
+train_out = input[:, 1:2]
+
+train = np.array(input[:, 2:8])
